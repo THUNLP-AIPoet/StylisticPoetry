5692aa8c3c331ca657d558520fec6234326f5861
Xiaoyuan Yi <mtmoonyi@gmail.com>
2020-05-05 09:06:12 +0800

    updata Yun.py

--- Yun.py
+++ Yun.py
@@ -2,14 +2,14 @@
 import numpy as np
 import pickle
 class Yun():
-    
+
     def __init__(self):
         print("loading YunList.txt")
         self.yun_dic = {}
         #self.count = 0
         f = open("data/poemcorpus/YunList.txt",'r')
         lines = f.readlines()
-        #dk_count = 0
+
         for line in lines:
             line = line.strip().split(' ')
             for i in range(len(line)):
@@ -21,15 +21,21 @@ class Yun():
             else:
                 if not line[1] in self.yun_dic[line[0]]:
                     self.yun_dic[line[0]].append(line[1])
-                    #dk_count += 1
-                #print "Yun has double key %s"%(line[0])
-        #print dk_count
+
         self.poemyun = {}
         self.mulword_map = {}
         self.word_map = {}
-        #self.totalpoemlist()
-        self.mulyun()
-    
+
+        # please download the pingshui_amb.pkl file from
+        #  https://github.com/THUNLP-AIPoet/Datasets/tree/master/CRRD
+        #  and then move it into the data/other/ dir
+        fyun = open("data/other/pingshui_amb.pkl", "rb")
+        self.word_map = pickle.load(fyun,encoding='utf8')
+        self.mulword_map = pickle.load(fyun,encoding='utf8')
+        self.poemyun = pickle.load(fyun,encoding='utf8')
+        fyun.close()
+
+
     def getBatchYun(self,batchSen,ivocab, PAD_ID): #return a matrix [none,30+1]
         numLen = len(batchSen)
         numBatch = batchSen[0].shape[0]
@@ -49,7 +55,7 @@ class Yun():
             else:
                 batchYun[i,int(yun[0])] = 1.0
         return batchYun
-    
+
     def getYun(self, sen):
         if sen in self.poemyun:
             return self.poemyun[sen]
@@ -73,63 +79,6 @@ class Yun():
             #self.count += 1
             return ['0']
 
-    def mulyun(self):
-        import pickle
-        import os
-        if os.path.exists("data/pkl/yun.pkl"):
-            fyun = open("data/pkl/yun.pkl", "rb")
-            self.word_map = pickle.load(fyun,encoding='utf8')
-            self.mulword_map = pickle.load(fyun,encoding='utf8')
-            self.poemyun = pickle.load(fyun,encoding='utf8')
-            fyun.close()
-            return
-        self.yun_map = {}
-        count = 0
-        for key in self.yun_dic:
-            if len(self.yun_dic[key])>1:
-                count += 1
-                self.yun_map.update({
-                    key: {}
-                    })
-                for yun in self.yun_dic[key]:
-                    self.yun_map[key].update({
-                        yun:0
-                        })
-
-        self.totalpoemlist()
-        for word in self.yun_map:
-            max_times = -1
-            max_yun = ""
-            for yun in self.yun_map[word]:
-                if self.yun_map[word][yun] > max_times:
-                    max_times = self.yun_map[word][yun]
-                    max_yun = yun
-            self.word_map.update({
-                word:[max_yun]
-                })
-
-        count1 = 0
-        count2 = 0
-        delitem = []
-        for tw in self.mulword_map:
-            count1 += 1
-            if len(self.mulword_map[tw]) > 1:
-                count2 +=1
-
-                delitem.append(tw)
-        for item in delitem:
-            self.mulword_map.pop(item)
-        for tw in self.mulword_map:
-            if len(self.mulword_map[tw]) > 1:
-                print(tw)
-
-        fyun = open("data/pkl/yun.pkl", "wb")
-        pickle.dump(self.word_map, fyun)
-        pickle.dump(self.mulword_map, fyun)
-        pickle.dump(self.poemyun, fyun)
-        fyun.close()
-
-
     def updateyun(self, sen, yun):
         def update_mulword(mulword):
             mulword = mulword
@@ -140,7 +89,7 @@ class Yun():
                 self.mulword_map.update({
                     mulword:[yun]
                     })
-            
+
         word = sen[-1]
         word = word
         yun = yun[0]
@@ -150,7 +99,7 @@ class Yun():
             update_mulword(twoword)
             threeword = sen[-3]+sen[-2]+sen[-1]
             update_mulword(threeword)
-            
+
     def totalpoemlist(self):
         # load the corpus to make sure the model won't generate existing sentences
         f = open("data/poemcorpus/totaljiantipoems_change.txt",'r')
@@ -168,7 +117,7 @@ class Yun():
                 L = len(sen_list)
                 yun_list = []
                 for i in range(L):
-                    yun_list.append(self.getYun(sen_list[i])) 
+                    yun_list.append(self.getYun(sen_list[i]))
                 if L>=1:
                     tmp = yun_list[1]
                     for i in range(L/2):
@@ -191,7 +140,7 @@ class Yun():
                     poemyun.update({
                         sen_list[i]:yun_list[i]
                         })
-                    
+
                 sen_list = []
             else:
                 sen_list.append(line[0])
